9b1a8ec7d7c38c0169feec43cee8a45917af5fb0
Yujin Kim <best5537@gmail.com>
2019-11-26 19:09:52 +0900

    QR recognization data parsing
    1/2

--- app/src/main/java/double_slash/techtown/com/phoneosk/MainActivity.java
+++ app/src/main/java/double_slash/techtown/com/phoneosk/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +16,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.CaptureManager;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 
@@ -26,72 +26,71 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import double_slash.techtown.com.Phoneosk.R;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btnMain;
-
-    // URL 설정.
-    String url="http://hycurium.cafe24.com/phoneosk/menu.jsp";
-
-    EditText edtInput;
-
-    TextView txt;
-    TextView txtParsed;
-
-    //content value
+    public String storeID = null;
     ContentValues contentValues;
 
-    public String storeID;
+    String url="http://hycurium.cafe24.com/phoneosk/menu.jsp";
 
-//    ArrayList<String> MenuNameParsed = new ArrayList<>();
-//    ArrayList<String> MenuPriceParsed = new ArrayList<>();
 
-        @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnMain = (Button)findViewById(R.id.btnMain);
-
-        edtInput = (EditText)findViewById(R.id.edtInput);
-        txt = (TextView)findViewById(R.id.txt);
-        txtParsed = (TextView)findViewById(R.id.txtParsed);
-
         contentValues = new ContentValues();
+        Button button = (Button) findViewById(R.id.btnstart);
+        button.setOnClickListener(Click);
 
-//        String input = edt.getText().toString();   밖에서 이렇게 하면 안됨
+    }
 
-        btnMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    View.OnClickListener Click = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
 
-                String input = edtInput.getText().toString();
+                case R.id.btnstart:
+                    qr();
+
+            }
+        }
+    };
+
+    public void qr() {
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator.setCaptureActivity(QR_Activity_Start.class);
+        intentIntegrator.initiateScan();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (result != null) {
+            Log.d("in", "in");
+            if (result.getContents() == null) {
+                Log.d("null", "null");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+
+            } else {
+                Log.d("scanning", "scanning");
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+
+                String input = result.getContents();
                 storeID = input;
                 contentValues.put("key", input);
-
-
-                HttpAsyncTask httpAsyncTask = new HttpAsyncTask(url,contentValues, getApplicationContext());
+                HttpAsyncTask httpAsyncTask = new HttpAsyncTask(url, contentValues, getApplicationContext());
                 httpAsyncTask.execute();
 
             }
-        });
-
-//        findViewById(R.id.btnClick).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                String input = edtInput.getText().toString();
-//                contentValues.put("key", input);
-//
-//                HttpAsyncTask httpAsyncTask = new HttpAsyncTask(url,contentValues, getApplicationContext());
-//                httpAsyncTask.execute();
-//
-//            }
-//        });
-
+        } else {
+            Log.d("else", "else");
+            super.onActivityResult(requestCode, resultCode, intent);
+        }
     }
 
     class HttpAsyncTask extends AsyncTask<Void, Void, String> {
@@ -110,23 +109,23 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(Void... voids) {
-
             String result; // 요청 결과를 저장할 변수.
             RequestHttpUrlConnection requestHttpURLConnection = new RequestHttpUrlConnection();
             result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+            Log.d("data", values.toString());
+
             return result;
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-
+            Log.d("s", s);
             //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
-            Toast.makeText(context,s,Toast.LENGTH_LONG).show();
+            Toast.makeText(context, s, Toast.LENGTH_LONG).show();
             ArrayList<String> MenuNameParsed = new ArrayList<>();
             ArrayList<String> MenuPriceParsed = new ArrayList<>();
-
-//            txt.setText(s);
+            //  txt.setText(s);
 
             receiveArray(s, MenuNameParsed, MenuPriceParsed);
 
@@ -140,33 +139,31 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        private void receiveArray(String dataObject, ArrayList MenuNameParsed, ArrayList MenuPriceParsed){
+        private void receiveArray(String dataObject, ArrayList MenuNameParsed, ArrayList MenuPriceParsed) {
 
 
             try {
                 // String 으로 들어온 값 JSONObject 로 1차 파싱
                 JSONObject wrapObject = new JSONObject(dataObject);
-
                 // JSONObject 의 키 "list" 의 값들을 JSONArray 형태로 변환
                 JSONArray jsonArray = new JSONArray(wrapObject.getString("ITEMS"));
-
-                for(int i = 0; i < jsonArray.length(); i++){
+                for (int i = 0; i < jsonArray.length(); i++) {
                     // Array 에서 하나의 JSONObject 를 추출
                     JSONObject dataJsonObject = jsonArray.getJSONObject(i);
                     // 추출한 Object 에서 필요한 데이터를 표시할 방법을 정해서 화면에 표시
                     // 필자는 RecyclerView 로 데이터를 표시 함
-//                mItems.add(new Item(dataJsonObject.getString("nation")+i,dataJsonObject.getString("name")+i,
-//                        dataJsonObject.getString("address")+i,dataJsonObject.getString("age")));
+                    //             mItems.add(new Item(dataJsonObject.getString("nation")+i,dataJsonObject.getString("name")+i,
+                    //                     dataJsonObject.getString("address")+i,dataJsonObject.getString("age")));
 
-                    MenuNameParsed.add(i, dataJsonObject.getString("menuName"));
+                    MenuNameParsed.add(i, dataJsonObject.getString("menuname"));
                     MenuPriceParsed.add(i, dataJsonObject.getString("price"));
                 }
                 // Recycler Adapter 에서 데이터 변경 사항을 체크하라는 함수 호출
-//            adapter.notifyDataSetChanged();
+                //           adapter.notifyDataSetChanged();
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
-
     }
+
 }
--- app/src/main/java/double_slash/techtown/com/phoneosk/QR_Activity_Start.java
+++ app/src/main/java/double_slash/techtown/com/phoneosk/QR_Activity_Start.java
@@ -1,4 +1,85 @@
 package double_slash.techtown.com.phoneosk;
 
-public class QR_Activity_Start {
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+
+import double_slash.techtown.com.Phoneosk.R;
+
+public class QR_Activity_Start extends AppCompatActivity {
+
+    ImageView btnBack_QR;
+//        public String storeID;
+//        ContentValues contentValues;
+//        String url = "http://hycurium.cafe24.com/minimenuProto/jsontest.jsp";
+
+
+    private CaptureManager capture;
+    private DecoratedBarcodeView barcodeScannerView;
+    //   public Bundle savedBundle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr___start);
+
+        btnBack_QR = (ImageView) findViewById(R.id.btnBack1_QR);
+
+        btnBack_QR.setOnClickListener(Click);
+
+        barcodeScannerView = (DecoratedBarcodeView) findViewById(R.id.scanner1_QR);
+
+        capture = new CaptureManager(this, barcodeScannerView);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.decode();
+
+
+    }
+
+    View.OnClickListener Click = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btnBack_QR:
+                    onBackPressed();
+                    break;
+            }
+        }
+    };
+
+    public void onResume() {
+        super.onResume();
+        capture.onResume();
+
+    }
+
+
+
 }
